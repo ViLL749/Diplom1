cd2e94c99e2e612a6abcee57bcf750c2fe115797
Vasilii <mihalcukv552@gmail.com>
2026-06-20 06:10:27 +1100

    Убраны Группы из Django admin

--- mykursovik2/accounts/admin.py
+++ mykursovik2/accounts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 admin.site.unregister(User)
+admin.site.unregister(Group)
 
 
 @admin.register(User)
